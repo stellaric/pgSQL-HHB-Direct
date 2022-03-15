f34e36630a053f3be57a6d35c5b480f1627d7fa2
stellaric <stella.richal.sio@gmail.com>
2022-03-15 16:50:00 +0100

    version 2

--- p1-hhb-direct.sql
+++ p1-hhb-direct.sql
@@ -1,8 +1,10 @@
 
-CREATE OR REPLACE FUNCTION getOneTuple() RETURNS record 
+CREATE OR REPLACE FUNCTION getOneTuple() 
+RETURNS record 
 LANGUAGE plpgsql
 AS
 $function$
+
     declare
         res record ;
     begin
@@ -15,7 +17,8 @@ $function$;
 CREATE OR REPLACE FUNCTION getOneTupleTable() 
 RETURNS SETOF client 
 LANGUAGE plpgsql
-AS $function$
+AS 
+$function$
     begin 
         return query select * from client ;
     end;
@@ -35,4 +38,35 @@ declare
             end loop ;
             return;
         end;
+    $function$;
+
+CREATE OR REPLACE FUNCTION nb_operation_compte_mois()
+RETURNS  INTEGER 
+LANGUAGE plpgsql
+AS $function$
+DECLARE 
+  num_mois INTEGER ;
+  num_annee INTEGER ;
+  num_compte INTEGER ;
+  id_type INTEGER ;
+
+    begin 
+    raise info 'debut fonction'
+    Select Into num_mois , num_annee , num_compte, id_type count(id_operation) from operation 
+    Inner Join compte on num_compte.compte=num_compte.operation 
+    Where
+  
+    raise info 'fin fonction'
+    end;
+
     $function$
+
+
+CREATE OR REPLACE FUNCTION creer_id_internet()
+RETURNS SETOF client
+LANGUAGE plpgsql ;
+AS $function$
+DECLARE
+id_internet char(1);
+nom_client character varying(30);
+prenom_client character varying(30);
